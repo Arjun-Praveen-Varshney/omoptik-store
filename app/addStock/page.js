b0d90dba1bd487eb11a6ee7726659a99a2953d69
Arjun <arjun.varshney1423@gmail.com>
2024-01-02 13:34:50 +0530

    minor changes in UI

--- app/addStock/page.js
+++ app/addStock/page.js
@@ -132,7 +132,7 @@ const addStock = () => {
               className="block text-gray-700 text-sm font-bold mb-2"
               htmlFor="add"
             >
-              Add (2 decimal)
+              Add (2 decimals)
             </label>
             <input
               className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
